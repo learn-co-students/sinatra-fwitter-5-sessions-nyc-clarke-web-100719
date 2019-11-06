cd6d56a7c475267efa3a28f4e08bcaf69355131b
Jimmy Cheung <jimmy.h.cheung@gmail.com>
2019-11-06 12:20:00 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,18 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "fwitter_secret"
+  end
+
+  helpers do
+    def logged_in?
+      session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
   end
 
   get '/' do
@@ -14,7 +26,25 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/tweet' do
-    erb :tweet
+    if session[:user_id] #returns nil if there isn't one
+      erb :tweet
+    else
+      redirect "/login"
+    end
+  end
+
+  get '/login' do
+    erb :login
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user
+      session[:user_id] = user.index
+      redirect "/"
+    else
+      redirect "/signup"
+    end
   end
 
   post '/tweet' do
@@ -33,5 +63,10 @@ class ApplicationController < Sinatra::Base
     @user = User.new(:username => params[:username], :email => params[:email])
     @user.save
     redirect '/'
+  end
+
+  get '/logout' do
+    session.destroy
+    redirect '/login'
   end
 end
